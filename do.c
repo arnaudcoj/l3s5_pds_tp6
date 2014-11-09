fa90f31019650c5d1e83af8f7987ebc2fcf8432b
arnaudcoj <arnaud.cojez@etudiant.univ-lille1.fr>
2014-11-09 23:24:31 +0100

    les options and or cc kill sont reconnues

--- do.c
+++ do.c
@@ -4,25 +4,80 @@
 #include <sys/wait.h>
 #include "makeargv.h"
 #include <errno.h>
+#include <getopt.h>
 
+enum opts {OR=0, AND=1, CC=2, KILL=4};
+
+int getargs(int argc, char **argv);
+int m_do (int argc, char **argv, int opts);
+void testmakeargv(int argc, char **argv);
 
 int main (int argc, char **argv)
+{
+  int opts;
+  opts = getargs(argc, argv);
+  /* testmakeargv(argc, argv);*/
+  if(opts == 63)
+    exit(EXIT_FAILURE);
+  printf("%d\n", opts);
+  return 0;
+  /*  return m_do(argc, argv, opts);*/
+}
+
+int getargs(int argc, char **argv)
+{
+  int val;
+  int tmp;
+  /* on définit les options qu'on veut trouver*/
+  struct option opts[5] = {
+    {"and", no_argument, NULL, AND},
+    {"or", no_argument, NULL, OR},
+    {"cc", no_argument, NULL, CC},
+    {"kill", no_argument, NULL, KILL},
+    {0,0,0,0}
+  };
+  tmp = val = 0;
+  /*tq il y a des options, on les ajoute à val*/
+  while (tmp != -1)
+    {
+      val |= tmp;
+      tmp = getopt_long(argc, argv, "", opts, NULL);
+    }
+  return val;
+}
+
+void testmakeargv(int argc, char **argv)
+{
+  char **lol;
+  int nbt, i, j;
+  lol = NULL;
+  for(i = 0; i < argc; i++)
+    {
+      nbt = makeargv(argv[i], " ", &lol);
+      for(j = 0; j < nbt; j++)
+	printf("%d:%s ", j, lol[j]);
+      printf("\n");
+      freeargv(lol);
+    }
+  return;
+}
+
+int m_do (int argc, char **argv, int opts)
 {
   int i;
   pid_t pid ;
   char *args[2];
   int statusfinal;
   int status;
-  int or;
-  or = 0;
   i = 1;
+  pid = 1;
 
   /*
     on vérifie si on a assez d'arguments
   */
   if(argc <= 1)
     {
-      perror("do : Pas assez d'arguments");
+      printf("Pas assez d'arguments\n");
       exit(EXIT_FAILURE);
     }
 
@@ -43,6 +98,7 @@ int main (int argc, char **argv)
 	      perror("erreur fork");
 	      exit(EXIT_FAILURE);
 	    case 0: /* fils : execute une commande */
+	      printf("exec : %s\n", args[0]);
 	      execvp(argv[i], args);
 	      break;
 	    default:
@@ -61,10 +117,10 @@ int main (int argc, char **argv)
   for(i = 2; i < argc; i++)
     {
       wait(&status);
-      if(or)
-	statusfinal |= WEXITSTATUS(status);
-      else
+      if(opts & AND)
 	statusfinal &= WEXITSTATUS(status);
+      else
+	statusfinal |= WEXITSTATUS(status);
     }
 
   return statusfinal;
